da57142dcaa22f5b21c7f64feb55625ec8e6ec40
Yana <petite.chu@gmail.com>
2016-07-23 21:37:30 +0300

    Updated to 2nd part of React.js and Spring Data REST tutorial

--- src/main/java/orbs/DatabaseLoader.java
+++ src/main/java/orbs/DatabaseLoader.java
@@ -16,5 +16,10 @@ public class DatabaseLoader implements CommandLineRunner {
 	@Override
 	public void run(String... strings) throws Exception {
 		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+		this.repository.save(new Employee("bilbo", "Baggins", "burglar"));
+		this.repository.save(new Employee("Gandalf", "the Grey", "wizard"));
+		this.repository.save(new Employee("Samwise", "Gamgee", "gardener"));
+		this.repository.save(new Employee("Meriadoc", "Brandybuck", "pony rider"));
+		this.repository.save(new Employee("Peregrin", "Took", "pipe smoker"));
 	}
 }
