b578335501fb0fd483fac06e818084c235525034
Mikhail Fesenko <proggga@gmail.com>
2016-08-12 12:56:10 +0300

    renamed Orb and SpellType to OrbEnum and SpellTypeEnum

--- src/main/java/orbs/models/enums/OrbEnum.java
+++ src/main/java/orbs/models/enums/OrbEnum.java
@@ -1,7 +1,7 @@
 package orbs.models.enums;
 
 // Orb from whose the spell is composed
-public enum Orb {
+public enum OrbEnum {
     EARTH,
     WIND,
     WATER,
--- src/main/java/orbs/models/enums/SpellTypeEnum.java
+++ src/main/java/orbs/models/enums/SpellTypeEnum.java
@@ -1,7 +1,7 @@
 package orbs.models.enums;
 
 // Todo: add more spells
-public enum SpellType {
+public enum SpellTypeEnum {
     Firebal, // fire, fire, fire
     Shield   // earth, earth, earth
 }
